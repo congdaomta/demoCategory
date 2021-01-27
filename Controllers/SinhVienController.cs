dcc04aac98e133158b2b03fca217a0bf69a583df
CongDao <congdaomta@gmail.com>
2021-01-27 13:34:32 +0700

    create sv

--- Controllers/SinhVienController.cs
+++ Controllers/SinhVienController.cs
@@ -1,4 +1,5 @@
 using demoCatalogue.Data;
+using demoCatalogue.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Encodings.Web;
@@ -14,9 +15,28 @@ namespace demoCatalogue.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
         public async Task<IActionResult> ListSinhVien()
         {
             return View(await _context.SinhVien.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CreateSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSV(SinhVien sv)
+        {
+            if(ModelState.IsValid){
+                _context.Add(sv);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("ListSinhVien");
+            }
+            return View(sv);
         }
     }
 }
